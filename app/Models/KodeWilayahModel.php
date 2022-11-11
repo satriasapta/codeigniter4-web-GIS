43b41d98d5515e78558f175f22d269a6aaec1d88
Satria Sapta <satriasapta48@gmail.com>
2022-11-11 22:31:52 +0700

    first commit

---
+++ app/Models/KodeWilayahModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class KodeWilayahModel extends Model
+{
+    protected $table = 'kode_wilayah';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+        'id', 'kode_wilayah','nama_wilayah'
+    ];
+    protected $returnType = 'App\Entities\KodeWilayah';
+    protected $useTimestamps = false;
+}
